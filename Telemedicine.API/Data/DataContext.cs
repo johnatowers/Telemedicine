0b149324b0a6ec9d708d602ee2bb16cbfd1de16d
Alex Towers <johnatowers@gmail.com>
2019-09-10 10:56:18 -0400

    Initial Commit

---
+++ Telemedicine.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Telemedicine.API.Models;
+
+namespace Telemedicine.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+        
+        public DbSet<Value> Values { get; set; }
+    }
+}
