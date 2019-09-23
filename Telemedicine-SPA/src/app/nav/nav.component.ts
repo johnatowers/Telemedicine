63292f864266926d2f8713dc945fbb5d89f684ef
Alex Towers <johnatowers@gmail.com>
2019-09-23 17:54:42 -0400

    added semicolon

--- Telemedicine-SPA/src/app/nav/nav.component.ts
+++ Telemedicine-SPA/src/app/nav/nav.component.ts
@@ -11,8 +11,8 @@ import { Router } from '@angular/router';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService, 
-    private router: Router) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService,
+              private router: Router) { }
 
   ngOnInit() {
   }
@@ -34,7 +34,7 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('Logged out');
-    this.router.navigate(['/home'])
+    this.router.navigate(['/home']);
   }
 
 }
