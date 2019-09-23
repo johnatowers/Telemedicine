20bab782427fad9d3757e207e2882eb9a6022d55
Alex Towers <johnatowers@gmail.com>
2019-09-23 16:36:26 -0400

    syntax fix

--- Telemedicine-SPA/src/app/app.component.ts
+++ Telemedicine-SPA/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 export class AppComponent implements OnInit {
   jwtHelper = new JwtHelperService();
 
-  constructor(private authService: AuthService){}
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
     const token = localStorage.getItem('token');
