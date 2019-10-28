6539a0f176cfdf2f36c02321ca7ed2e9bb78994f
CooganKoerts <coogsk@gmail.com>
2019-10-28 19:44:41 -0400

    minor changes

--- Telemedicine-SPA/src/app/patient-appointments/patient-appointments.component.ts
+++ Telemedicine-SPA/src/app/patient-appointments/patient-appointments.component.ts
@@ -35,6 +35,7 @@ export class PatientAppointmentsComponent implements OnInit {
 
   viewDate: Date = new Date();
 
+  // This will eb the part that connects to the DB
   modalData: {
     action: string;
     event: CalendarEvent;
@@ -58,46 +59,46 @@ export class PatientAppointmentsComponent implements OnInit {
 
   refresh: Subject<any> = new Subject();
 
-  events: CalendarEvent[] = [
-    {
-      start: subDays(startOfDay(new Date()), 1),
-      end: addDays(new Date(), 1),
-      title: 'A 3 day event',
-      color: colors.red,
-      actions: this.actions,
-      allDay: true,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true
-      },
-      draggable: true
-    },
-    {
-      start: startOfDay(new Date()),
-      title: 'An event with no end date',
-      color: colors.yellow,
-      actions: this.actions
-    },
-    {
-      start: subDays(endOfMonth(new Date()), 3),
-      end: addDays(endOfMonth(new Date()), 3),
-      title: 'A long event that spans 2 months',
-      color: colors.blue,
-      allDay: true
-    },
-    {
-      start: addHours(startOfDay(new Date()), 2),
-      end: new Date(),
-      title: 'A draggable and resizable event',
-      color: colors.yellow,
-      actions: this.actions,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true
-      },
-      draggable: true
-    }
-  ];
+  events: CalendarEvent[] = [];
+
+  // {
+  //   start: subDays(startOfDay(new Date()), 1),
+  //   end: addDays(new Date(), 1),
+  //   title: 'A 3 day event',
+  //   color: colors.red,
+  //   actions: this.actions,
+  //   allDay: true,
+  //   resizable: {
+  //     beforeStart: true,
+  //     afterEnd: true
+  //   },
+  //   draggable: true
+  // },
+  // {
+  //   start: startOfDay(new Date()),
+  //   title: 'An event with no end date',
+  //   color: colors.yellow,
+  //   actions: this.actions
+  // },
+  // {
+  //   start: subDays(endOfMonth(new Date()), 3),
+  //   end: addDays(endOfMonth(new Date()), 3),
+  //   title: 'A long event that spans 2 months',
+  //   color: colors.blue,
+  //   allDay: true
+  // },
+  // {
+  //   start: addHours(startOfDay(new Date()), 2),
+  //   end: new Date(),
+  //   title: 'A draggable and resizable event',
+  //   color: colors.yellow,
+  //   actions: this.actions,
+  //   resizable: {
+  //     beforeStart: true,
+  //     afterEnd: true
+  //   },
+  //   draggable: true
+  // }
 
   activeDayIsOpen = true;
 
