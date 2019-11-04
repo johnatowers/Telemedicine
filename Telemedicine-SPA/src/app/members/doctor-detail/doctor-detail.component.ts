f214d61c7cf51fe13125f81512182b5ada1d5799
Macy Parker <macyvparker@gmail.com>
2019-11-04 17:48:03 -0500

    Patient and doctor "charts"

---
+++ Telemedicine-SPA/src/app/members/doctor-detail/doctor-detail.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TabsetComponent } from 'ngx-bootstrap';
+import { User } from 'src/app/_models/user';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-doctor-detail',
+  templateUrl: './doctor-detail.component.html',
+  styleUrls: ['./doctor-detail.component.css']
+})
+export class DoctorDetailComponent implements OnInit {
+  @ViewChild('memberTabs', {static: true}) memberTabs: TabsetComponent;
+  user: User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+
+  constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      // tslint:disable-next-line: no-string-literal
+      this.user = data['user'];
+    });
+
+    this.route.queryParams.subscribe(params => {
+      const selectedTab = params['tab'];
+      this.memberTabs.tabs[selectedTab > 0 ? selectedTab : 0].active = true;
+    });
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];
+    this.galleryImages = [];
+  }
+
+  getImages() {
+    const imageUrls = [];
+    for (const doc of this.user.documents) {
+      imageUrls.push({
+        small: doc.url,
+        medium: doc.url,
+        big: doc.url,
+        description: doc.description
+      });
+    }
+
+    return imageUrls;
+  }
+
+  selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true;
+  }
+
+}
