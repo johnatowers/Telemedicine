bfc823601bc82902e322b30a6005ce7f062875d1
CooganKoerts <coogsk@gmail.com>
2019-11-15 22:18:43 -0500

    Prod Deployment 11.15.19

--- Telemedicine-SPA/src/app/patient-appointments/patient-appointments-util/patient-appointments-util.module.ts
+++ Telemedicine-SPA/src/app/patient-appointments/patient-appointments-util/patient-appointments-util.module.ts
@@ -2,11 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CalendarModule } from 'angular-calendar';
-import { CalendarHeaderComponent } from './patient-appointments-util.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, CalendarModule],
-  declarations: [CalendarHeaderComponent],
-  exports: [CalendarHeaderComponent]
+  imports: [CommonModule, FormsModule, CalendarModule]
 })
 export class PatientAppointmentsUtilsModule {}
